a53a3b8f82860ff77254bef5e2ec59653af4fffa
Nokihera <minzarnihtut811@gmail.com>
2024-08-10 11:47:00 +0700

    category filter working-on

--- react-shopping-web/src/components/Categories.jsx
+++ react-shopping-web/src/components/Categories.jsx
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryButton from "./CategoryButton";
 
 const Categories = () => {
-  const category = [
-    "Electronics",
-    "Men's clothes",
-    "Woman's clothes",
-    "Jewelry",
-  ];
+  const [Category, setCategory] = useState([
+    { cate: "All", showOff: true, id: 0 },
+    { cate: "Electronics", showOff: false, id: 1 },
+    { cate: "Men's clothes", showOff: false, id: 2 },
+    { cate: "Woman's clothes", showOff: false, id: 3 },
+    { cate: "Jewelry", showOff: false, id: 4 },
+  ]);
+  const handleChangeShowOff = (id) => {
+    setCategory(Category.map((el) =>
+        el.id === id ? { ...el, showOff: !el.showOff } : el
+    ));
+};
   return (
     <div className="flex gap-4 pb-3 container mx-auto flex-nowrap overflow-scroll categories-btn-gp">
-      {category.map((category) => (
-        <CategoryButton key={category} name={category} />
+      
+      {Category.map((el) => (
+        <CategoryButton key={el.id} name={el} handleChangeShowOff={handleChangeShowOff} />
       ))}
     </div>
   );
--- react-shopping-web/src/components/CategoryButton.jsx
+++ react-shopping-web/src/components/CategoryButton.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
-const CategoryButton = ({ name }) => {
+const CategoryButton = ({ name: { cate, showOff,id }, handleChangeShowOff }) => {
+  
+  const handleOnCLick = ()=> {
+    handleChangeShowOff(id)
+  }
   return (
-    <button className="border-2 border-black px-4 py-2 font-semibold text-nowrap">
-      {name}
-    </button>
+    <>
+      <button className={`border-2 border-black px-4 py-2 font-semibold text-nowrap ${showOff ? "bg-black text-white" : "bg-white text-black"} transition-all duration-300`} onClick={handleOnCLick} >
+        {cate}
+      </button>
+    </>
   );
 };
 
