e3c8e9a8a8ceb80651fe041f663c83a05fed3daa
Nokihera <minzarnihtut811@gmail.com>
2024-08-12 14:11:23 +0700

    NavBar Working on

--- design-project-vite/src/components/Heading.jsx
+++ design-project-vite/src/components/Heading.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import LOGO from '../assets/LOGO.svg'
+import NavBtn from './NavBtn';
 
 const Heading = () => {
+  const navButtons=[
+    {id:1, name:"Home", isActive:true},
+    {id:2, name:"About", isActive:false},
+    {id:3, name:"Blog", isActive:false},
+    {id:4, name:"Contact", isActive:false},
+  ];
+
   return (
-    <div className='py-5 bg-white shadow-lg border-[1px] border-[#5F6D7E66] h-[72px]'>Heading</div>
+    <div className='py-5 bg-white shadow-lg border-[1px] border-[#5F6D7E66] h-[72px] px-5 flex justify-between'>
+      <img src={LOGO} alt="" />
+      <div>
+      {navButtons.map((item)=>(<NavBtn key={item.id} name={item.name} isActive={item.isActive}/>))}
+      </div>
+    </div>
   )
 }
 
